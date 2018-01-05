ccf2b3dbe7e8961fe7930306eea75316c60e4182
Stefan Jansson <s.jansson@gmail.com>
2018-01-05 11:00:58 +0100

    Tests with queryParameters defined in given() with queryParam instead of url

---
+++ src/test/TestQRCode.java
@@ -0,0 +1,50 @@
+import static io.restassured.RestAssured.*;
+import org.junit.Test;
+
+public class TestQRCode {
+    String baseUrl = "http://api.qrserver.com/v1";
+
+    @Test
+    public void CreateQRCodeWithMostBasicRequest() {
+        given().
+            queryParam("data", "myDataForQRCode").  //mandatory
+        when().
+            post(baseUrl + "/create-qr-code/").
+        then().
+            statusCode(200);
+    }
+
+    @Test
+    public void CreateQRCodeWithManyOptionalParameters() {
+        given().
+            queryParam("data", "myDataForQRCode").  //mandatory
+            queryParam("size","10x10").             //optional
+            queryParam("charset-target","UTF-8").   //optional
+            queryParam("bgcolor","255-255-255").    //optional (white)
+            queryParam("color","255-0-0").          //optional (red)
+            queryParam("format","gif").             //optional
+        when().
+            post(baseUrl + "/create-qr-code/").
+        then().
+            statusCode(200);
+    }
+
+    @Test
+    public void CreateQRCodeWithNoParameters_500InternalServerError() {
+        given().
+        when().
+            post(baseUrl + "/create-qr-code/").
+        then().
+            statusCode(500);
+    }
+
+    @Test
+    public void CreateQRCodeMissingMandatoryParameter_400BadRequest() {
+        given().
+            queryParam("size","10x10").             //optional
+        when().
+            post(baseUrl + "/create-qr-code/").
+        then().
+            statusCode(400);
+    }
+}
